b9d61978c6a8f7f10565085268d6127af49fc92e
sajithgithub <sajith.developer@gmail.com>
2013-06-28 14:09:02 +0530

    gg

---
+++ picnicc.js
@@ -0,0 +1,21 @@
+/* {picnicc:_do-not-minify}  */
+/**
+* picnicc.js v1.1.3
+* @author Alex Duloz ~ @alexduloz ~ http://bitspushedaround.com/
+* MIT license
+*/
+/* { / picnicc }  */
+(function(window) {
+
+	var document = window.document;
+
+	var self = blueprint = {
+
+		_: function() {
+			return this;
+		},
+	}
+
+	blueprint._();
+	window.picnicc = blueprint;
+})(window);
